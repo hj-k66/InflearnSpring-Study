07261bc27742dd0093425a13294722ca18b4485f
heejeong <hjx66@naver.com>
2022-07-21 14:34:45 +0900

    [스프링 DB 접근 기술] JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,25 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     private final DataSource dataSource;
+    private final EntityManager em;
 
-    public SpringConfig(DataSource dataSource){
+
+    public SpringConfig(DataSource dataSource, EntityManager em){
         this.dataSource = dataSource;
+        this.em = em;
     }
     @Bean
     public MemberService memberService(){
@@ -27,7 +31,8 @@ public class SpringConfig {
 
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
